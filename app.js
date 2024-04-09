0d0492b37abb31d1d749a4bc938682f25776d390
SomeName <Some@gamil.com>
2024-04-10 03:58:13 +0530

    some file changes

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// git init: first command to start a new GIT project
+
+// git status: check current status of GIT files
+
+// git add <filename>: For staging files for next commit
+
+// git add . : Selecting all files for staging
+
+// git commit -m <message> : For saving code to Git project
+
+// git log: review and read a history of everything to a repository
+
+// git branch : Shows list of all branchs (* active branch)
+
+// git diff --cached: difference between current staged and previous commit
+
+
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('other file');
+console.log('Git project');
+
+const a = 5;
+const b = 4;
